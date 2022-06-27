279bec45a094ebb1df84bec662223d7df9bc2abb
Aruka Torres <arukatorres@gmail.com>
2022-06-27 14:27:54 +0200

    100 days of code Python, day 6

---
+++ days_1-10/day6 (Functions)/Functions and while loop.py
@@ -0,0 +1,14 @@
+# Definición de funciones
+def my_function():
+    print("Hello")
+    print("Bye")
+
+my_function()
+
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+
+# While
+cont = 6
+while cont > 0:
+    print(cont)
+    cont -= 1
